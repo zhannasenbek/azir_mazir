a59eabed61d8e38e76267a9d9ba1d90973a712d6
aiqyyn1 <ibrayevaiqyn@gmail.com>
2024-03-01 11:36:58 +0500

    feat:add navigation

--- src/components/__navbar/Navbar.jsx
+++ src/components/__navbar/Navbar.jsx
@@ -3,12 +3,23 @@ import styles from './navbar.module.css';
 import user from '../../assets/user.png';
 import heart from '../../assets/heart.png';
 import logo from '../../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
+  let navigate = useNavigate();
+  const handleNavigatePath = (path) => navigate(path);
   return (
     <div>
       <header className={styles.main__header}>
-        <img src={logo} className={styles.main__logo}></img>
-        <img src={user} className={styles.main__userLogo} alt='User Logo' />
+        <img
+          src={logo}
+          className={styles.main__logo}
+          onClick={() => handleNavigatePath('/')}></img>
+        <img
+          src={user}
+          className={styles.main__userLogo}
+          onClick={() => handleNavigatePath('/register')}
+          alt='User Logo'
+        />
         <img
           src={heart}
           className={styles.main__heartLogo}
